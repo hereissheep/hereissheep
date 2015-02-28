cb1a826abcc3305431a04c79fdded85ee2e11cb3
helio <heliosouza@gmail.com>
2015-02-28 16:36:00 -0300

    commit for entity product

--- src/ApiBundle/Entity/Product.php
+++ src/ApiBundle/Entity/Product.php
@@ -8,7 +8,62 @@
 
 namespace ApiBundle\Entity;
 
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+use Doctrine\Common\Collections\ArrayCollection;
 
-class Product {
+class Product
+{
+
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="bigint", name="id_product")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * @ORM\Column(type="string", name="description")
+     */
+    protected $description;
+
+    /**
+     * @var Category
+     *
+     * @ORM\ManyToOne(targetEntity="Category", mappedBy="Category")
+     * @ORM\JoinColumn(name="category_id", referencedColumnName="category")
+     */
+    protected $category;
+
+    /**
+     * @var ArrayCollection
+     * @ORM\ManyToMany(targetEntity="User", inversedBy="products")
+     * @ORM\JoinTable(name="products_users")
+     **/
+    protected $users;
+
+    /**
+     * @var Location
+     *
+     * @ORM\OneToOne(targetEntity="Location", mappedBy="Location")
+     * @ORM\JoinColumn(name="location_id", referencedColumnName="location")
+     */
+    protected $location;
+
+    /**
+     * @ORM\Column(type="date", name="date_post")
+     */
+    protected $date;
+
+    /**
+     * @ORM\Column(type="decimal", name="price")
+     */
+    protected $price;
+
+    public function __construct()
+    {
+        $this->users = new ArrayCollection();
+    }
+
 
 }
