93b8272710c8895e68de5e13cbd064f12f677e2a
helio <heliosouza@gmail.com>
2015-02-28 16:49:02 -0300

    Entity user

--- src/ApiBundle/Entity/User.php
+++ src/ApiBundle/Entity/User.php
@@ -19,5 +19,89 @@ class User
      */
     protected $id;
 
+    /**
+     * @ORM\userName
+     * @ORM\Column(type="string", mane="name")
+     */
+    protected $userName;
+
+    /**
+     * @ORM\password
+     * @ORM\Column(type="string", name="password")
+     */
+    protected $password;
+
+    /**
+     * @ORM\email
+     * @ORM\Column(type="string", name="email")
+     */
+    protected $email;
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUserName()
+    {
+        return $this->userName;
+    }
+
+    /**
+     * @param mixed $userName
+     */
+    public function setUserName($userName)
+    {
+        $this->userName = $userName;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * @param mixed $password
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param mixed $email
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+
+    
+
 
 }
