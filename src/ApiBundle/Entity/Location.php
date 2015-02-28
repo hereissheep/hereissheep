ab334079e57279441f142d97063555a0c268bbad
helio <heliosouza@gmail.com>
2015-02-28 17:17:09 -0300

    Entity location

---
+++ src/ApiBundle/Entity/Location.php
@@ -0,0 +1,105 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: helio
+ * Date: 28/02/15
+ * Time: 16:50
+ */
+
+namespace ApiBundle\Entity;
+
+
+class Location
+{
+
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="bigint", name="location_id")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * @ORM\Column(type="string", name="address")
+     */
+    protected $address;
+
+    /**
+     * @ORM\Column(type="string", name="latitude")
+     */
+    protected $latitude;
+
+    /**
+     * @ORM\Column(type="string", name="longitude")
+     */
+    protected $longitude;
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAddress()
+    {
+        return $this->address;
+    }
+
+    /**
+     * @param mixed $address
+     */
+    public function setAddress($address)
+    {
+        $this->address = $address;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getLatitude()
+    {
+        return $this->latitude;
+    }
+
+    /**
+     * @param mixed $latitude
+     */
+    public function setLatitude($latitude)
+    {
+        $this->latitude = $latitude;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getLongitude()
+    {
+        return $this->longitude;
+    }
+
+    /**
+     * @param mixed $longitude
+     */
+    public function setLongitude($longitude)
+    {
+        $this->longitude = $longitude;
+    }
+
+
+
+
+
+}
